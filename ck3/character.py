ca12f3dadad8b73447eed27eaa619229283a0dbd
Richard Braakman <shuttlegit@gmail.com>
2022-06-15 12:28:45 +0300

    Add fornication counting tool

--- ck3/character.py
+++ ck3/character.py
@@ -4,19 +4,40 @@ class Character:
     def __init__(self, id, state):
         self.id = id
         self.state = state
+        self.female = False
         self.death_date = None
         self.birth_date = None
         self.title_ids = []
+        self.child_ids = []
+        self.real_father_id = None
+        self.trait_ids = []
+        self.flags = []
+        self.primary_spouse_id = None
+        self.siblings = []  # derived later
 
     def assign(self, key, token):
-        if key == 'dead_data/date':
+        if key == 'female':
+            self.female = token.value == 'yes'
+        elif key == 'dead_data/date':
             self.death_date = token.value
         elif key == 'birth':
             self.birth_date = token.value
+        elif key == 'family_data/real_father':
+            self.real_father_id = token.value
+        elif key == "alive_data/variables/data//flag":
+            self.flags.append(token.value)
+        elif key == 'family_data/primary_spouse':
+            self.primary_spouse_id = token.value
 
     def value(self, key, token):
         if key == 'landed_data/domain':
             self.title_ids.append(token.value)
+        elif key == 'family_data/child':
+            self.child_ids.append(token.value)
+        elif key == 'traits':
+            self.trait_ids.append(token.value)
+        elif key == 'alive_data/variables/data//flag':
+            self.flags.append(token.value)
 
     def alive(self):
         return self.death_date is None
@@ -32,3 +53,16 @@ class Character:
 
     def landed(self):
         return any(t.rank == Rank.Barony for t in self.titles())
+
+    def children(self):
+        return [ self.state.characters[c_id] for c_id in self.child_ids ]
+
+    def real_children(self):
+        if self.female:
+            return self.children()
+        children = [ c for c in self.children()
+                     if c.real_father_id is None or c.real_father_id == self.id ]
+        for c in self.state.characters.values():
+            if c.real_father_id == self.id and c not in children:
+                children.append(c)
+        return children
